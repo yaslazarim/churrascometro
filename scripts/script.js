2b0d79675292016497792ab583f80b703fa0e3ac
Yasmim Lazarim <yasslazarim@gmail.com>
2025-03-25 10:46:27 -0300

    Change configs button

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,20 @@ let inputDuracao = document.getElementById("duracao");
 
 let resultado = document.getElementById("resultado");
 
+document.querySelector('.calc-button').addEventListener('click', calcular)
+
 function calcular(){
     let adultos = inputAdultos.value;
     let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
 
     let qdtTotalCarne = carnePorPessoa(duracao) * adultos + (carnePorPessoa(duracao) / 2 * criancas);
     let qdtTotalCerveja = carnePorPessoa(duracao) * adultos;
     let qdtTotalBebidas = bebidasPorPessoa(duracao) * adultos + (bebidasPorPessoa(duracao) / 2 * criancas);
 
+    resultado.innerHTML = `<p>${qdtTotalCarne/1000} Kg de carne</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalCerveja/355)} latas de cerveja</p>`
+    resultado.innerHTML += `<p>${qdtTotalBebidas/1000}ml de bebidas</p>`
 }
 
 function carnePorPessoa(duracao){
